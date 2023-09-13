47ae088faac8eca56048bb12c114fea58621180d
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-09-13 15:47:32 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -37,26 +37,32 @@ function App() {
       } catch (error) {
         setToken(null)
         // navigate to login page
-        return console.log('Token validation error:', error)
+        return console.log("Token validation error:", error)
       }
     }
 
     if (token) {
       isTokenValid()
     }
-
   }, [token])
 
   return (
     <>
       <Routes>
-        <Route
+        <Route path="/login" element={<Login />} />
+        {/* <Route
           path="/login"
           element={!token ? <Login /> : <Navigate to="/" />}
-        />
+        /> */}
         <Route
           path="/"
-          element={token ? <Home userData={userData} setToken={setToken} /> : <Navigate to="/login" />}
+          element={
+            token ? (
+              <Home userData={userData} setToken={setToken} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/login" />} />
